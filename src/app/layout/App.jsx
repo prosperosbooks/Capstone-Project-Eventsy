779c2980ee7f3529f7b4567f1fa83b29feb7c7d3
Emin <allen.emin@gmail.com>
2019-03-08 07:40:39 -0500

    set up event dashboard ui and routed props to submit create event forms

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Eventsy</h1>
+        <NavBar>
+          <Container>
+            <h1>Eventsy</h1>
+          </Container>
+        </NavBar>
+        <EventDashboard />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./app/layout/App";
-
 
 const rootEl = document.getElementById("root");
 
@@ -17,4 +17,3 @@ if (module.hot) {
 }
 
 render();
-
