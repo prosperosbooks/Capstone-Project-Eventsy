088233ec0f8d406cec782b2b7dfd0d6308ca6916
Emin <allen.emin@gmail.com>
2019-03-18 22:09:27 -0400

    configured authentication front-end UI, home page, and settings component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
-import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import { Route, Switch } from "react-router-dom";
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
+import EventDetailedPage from "../../features/events/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import UserDetailed from "../../features/user/UserDetailed/UserDetailedPage";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import EventForm from "../../features/events/EventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
+
 import NavBar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar>
-          <Container>
-            <h1>Eventsy</h1>
-          </Container>
-        </NavBar>
-        <EventDashboard />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile" component={UserDetailed} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
