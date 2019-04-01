4dccb1902731f8ba4b2b4fbbfa7c0de0c9cc13cf
Emin <allen.emin@gmail.com>
2019-03-31 21:28:49 -0400

    test redux actions and reducers, configured test component that increments / decrements state with hotloading

---
+++ src/features/testarea/TestComponent.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { incrementCounter, decrementCounter } from "./testActions";
+
+// receives state from store
+// test.data matches 'test' in root reducer
+const mapState = state => ({
+  data: state.test.data
+});
+
+const actions = {
+  incrementCounter,
+  decrementCounter
+};
+
+class TestComponent extends Component {
+  render() {
+    const { incrementCounter, decrementCounter, data } = this.props;
+    return (
+      <div>
+        <h1>Test Area</h1>
+        <h3>The answer is: {data}</h3>
+        <Button onClick={incrementCounter} color="green" content="Increment" />
+        <Button onClick={decrementCounter} color="red" content="Decrement" />
+      </div>
+    );
+  }
+}
+
+export default connect(
+  mapState,
+  actions
+)(TestComponent);
